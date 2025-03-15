9c6c07f2115fc375137e68b8cafff7982cd5cb71
SafalNarshing <safalnarsingh1@gmail.com>
2025-03-15 10:55:07 +0545

    updated for deployment

---
+++ sudokuSolver/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/hash
+
+# Install the required dependencies
+pip install setuptools
+pip install -r requirements.txt
+
+#Run django commands
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic 
